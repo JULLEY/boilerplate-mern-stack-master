ec91d0c2299e947b7f7ba7a68d0a9fb10868289b
woong <ung91@yahoo.com>
2021-01-12 00:33:06 +0900

    Load More버튼 위치 조정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,4 +3,4 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = '7314fa70d5246ee65cf253303d1bf7be';
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -20,14 +20,14 @@ function LandingPage() {
 
     const fetchMovies = (endpoint) => {
         fetch(endpoint)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response)
-                console.log(response.results)
-                setMovies([...Movies, ...response.results])
-                setMainMovieImage(response.results[0])
-                setCurrentPage(response.page)
-            })
+          .then((response) => response.json())
+          .then((response) => {
+            console.log(response);
+            console.log(response.results);
+            setMovies([...Movies, ...response.results]);
+            setMainMovieImage(response.results[0]);
+            setCurrentPage(response.page);
+          });
     }
 
     const loadMoreItems = () => {
@@ -36,44 +36,45 @@ function LandingPage() {
     }
 
     return (
-        <div style={{ width : '100%', margin: '0' }}>
-            
-            {/* Main Image */}
-            {MainMovieImage && 
-                <MainImage 
-                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
-                    title={MainMovieImage.original_title}
-                    text={MainMovieImage.overview}
-                />
-            }
-            <div style={{ width: '85%', margin: 'auto'}}>
+      <div style={{ width: "100%", margin: "0" }}>
+        {/* Main Image */}
+        {MainMovieImage && (
+          <MainImage
+            image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+            title={MainMovieImage.original_title}
+            text={MainMovieImage.overview}
+          />
+        )}
+        <div style={{ width: "85%", margin: "1rem auto" }}>
+          <h2>Movies by latest</h2>
+          <hr />
 
-                <h2>Movies by latest</h2>
-                <hr/>
+          {/* Movie Grid Cards */}
 
-                {/* Movie Grid Cards */}
-                <Row gutter={[16, 16]} >
-
-                    {Movies && Movies.map((movie, index) => (
-                        <React.Fragment key={index}>
-                            <GridCards
-                                image={movie.poster_path ?
-                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
-                                movieId={movie.id}
-                                movieName={movie.original_title}
-                            />
-                        </React.Fragment>
-                    ))}
-
-                </Row>
-
-            </div>
-
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button onClick={loadMoreItems}> Load More</button>
-            </div>
+          <Row gutter={[16, 16]}>
+            {Movies &&
+              Movies.map((movie, index) => (
+                <React.Fragment key={index}>
+                  <GridCards
+                    landingPage
+                    image={
+                      movie.poster_path
+                        ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                        : null
+                    }
+                    movieId={movie.id}
+                    movieName={movie.original_title}
+                  />
+                </React.Fragment>
+              ))}
+          </Row>
         </div>
-    )
+
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={loadMoreItems}> Load More</button>
+        </div>
+      </div>
+    );
 }
 
 export default LandingPage
