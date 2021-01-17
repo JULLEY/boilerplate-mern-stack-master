cb1cb6d3d35a711f989e795490f59b222f99073b
woong <ung91@yahoo.com>
2021-01-17 17:06:54 +0900

    Favorite button event API 완성

--- client/src/components/views/MovieDetail/Section/Favorite.js
+++ client/src/components/views/MovieDetail/Section/Favorite.js
@@ -4,52 +4,75 @@ import { Button } from 'antd';
 
 // {/* <Favorite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} /> 로 선언한 props들을 가져온다. */}
 function Favorite(props) {
+  const movieId = props.movieId;
+  const userFrom = props.userFrom;
+  const movieTitle = props.movieInfo.title;
+  const moviePost = props.movieInfo.backdrop_path;
+  const movieRunTime = props.movieInfo.runtime;
 
-    const movieId = props.movieId
-    const userFrom = props.userFrom
-    const movieTitle = props.movieInfo.title
-    const moviePost = props.movieInfo.backdrop_path
-    const movieRunTime = props.movieInfo.runtime
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
 
-    const [FavoriteNumber, setFavoriteNumber] = useState(0)
-    const [Favorited, setFavorited] = useState(false)
+  let variables = {
+    userFrom: userFrom,
+    movieId: movieId,
+    movieTitle: movieTitle,
+    moviePost: moviePost,
+    movieRunTime: movieRunTime,
+  };
 
-    useEffect(() => {
+  useEffect(() => {
+    // post안의 URL는 임의 지정
+    Axios.post("/api/favorite/favoriteNumber", variables).then((response) => {
+      console.log("favoriteNumber : ", response.data);
+      setFavoriteNumber(response.data.favoriteNumber);
+      if (response.data.success) {
+      } else {
+        alert("숫자 정보를 가져오는데 실패 했습니다.");
+      }
+    });
 
-        let variables = {
-            userFrom,
-            movieId
+    Axios.post("/api/favorite/favorited", variables).then((response) => {
+      console.log("favorited : ", response.data);
+      setFavorited(response.data.favorited);
+      if (response.data.success) {
+      } else {
+        alert("정보를 가져오는데 실패 했습니다.");
+      }
+    });
+  }, []);
+
+  const onClickFavorite = () => {
+    if (Favorited) {
+      Axios.post("/api/favorite/removeFromFavorite", variables).then(
+        (response) => {
+          if (response.data.success) {
+            setFavoriteNumber(FavoriteNumber - 1);
+            setFavorited(!Favorited);
+          } else {
+            alert("Favorite 리스트에서 지우는 걸 실패했습니다.");
+          }
         }
+      );
+    } else {
+      Axios.post("/api/favorite/addToFavorite", variables).then((response) => {
+        if (response.data.success) {
+          setFavoriteNumber(FavoriteNumber + 1);
+          setFavorited(!Favorited);
+        } else {
+          alert("Favorite 리스트에서 추가하는 걸 실패했습니다.");
+        }
+      });
+    }
+  };
 
-        // post안의 URL는 임의 지정
-        Axios.post('/api/favorite/favoriteNumber', variables)
-            .then(response => {
-                console.log("favoriteNumber : ", response.data)
-                    setFavoriteNumber(response.data.favoriteNumber)
-                if (response.data.success) {
-                } else {
-                    alert('숫자 정보를 가져오는데 실패 했습니다.')
-                }
-            })
-
-
-        Axios.post('/api/favorite/favorited', variables)
-            .then(response => {
-                console.log("favorited : " , response.data)
-                    setFavorited(response.data.favorited)
-                if (response.data.success) {
-                } else {
-                    alert('정보를 가져오는데 실패 했습니다.')
-                }
-            })
-
-    }, [])
-
-    return (
-        <div>
-            <button>{Favorited ? " Not Favorite" : "Add to Favorite "} {FavoriteNumber} </button>
-        </div>
-    )
+  return (
+    <div>
+      <Button onClick={onClickFavorite}>
+        {Favorited ? " Not Favorite" : "Add to Favorite "} {FavoriteNumber}{" "}
+      </Button>
+    </div>
+  );
 }
 
 export default Favorite
